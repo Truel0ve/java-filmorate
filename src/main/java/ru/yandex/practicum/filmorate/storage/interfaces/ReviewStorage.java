464e981ebf2cfa1de6afe2c6b65a1ddeccf69104
Ivan Nizalzov <ivan.nizalzov@gmail.com>
2023-03-06 22:46:37 +0300

    style: refactored some code

--- src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/ReviewStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/interfaces/ReviewStorage.java
@@ -5,6 +5,7 @@ import ru.yandex.practicum.filmorate.model.Review;
 import java.util.List;
 
 public interface ReviewStorage {
+
     Review createReview(Review review);
     void updateReview(Review review);
     void deleteReview(Long reviewId);
